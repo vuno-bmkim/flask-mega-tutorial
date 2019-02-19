6ebef18baa38b237eb987fa0485497aa1b2e0092
vuno-bmkim <bmkim@vuno.co>
2019-02-19 17:37:47 +0900

    Chapter 12: Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_login import LoginManager
 from config import Config
 
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
