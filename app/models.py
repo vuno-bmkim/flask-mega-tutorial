af44fb612f95ddecf7b09f227720079170ef0383
vuno-bmkim <bmkim@vuno.co>
2019-02-19 12:50:30 +0900

    Chapter 5: User Logins

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,6 +12,22 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        # forms를 통해 User 생성시 password없을 가능성 없지만, 
+        # flask shell을 통해 User 생성시 password가 없어 에러 발생 가능 
+        if self.password_hash is None:
+            print("User {} doesn't have password_hash".format(self.username))
+            return False
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
