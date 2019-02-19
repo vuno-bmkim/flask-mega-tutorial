9e33432368ede1776e42ee078627a7962f4f61e3
vuno-bmkim <bmkim@vuno.co>
2019-02-19 13:03:08 +0900

    Chapter 6: Profile Page and Avatars

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from flask_login import current_user
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -29,3 +30,16 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    # username은 unique 값으로 쓰이므로 EditProfileForm에서
+    # 기존 username에서 다른 username으로 변경한 경우에 대해 중복여부 검증 필요
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if current_user.username != username.data and user is not None:
+            raise ValidationError('Please use a different username.')
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+
 
 class User(UserMixin, db.Model):    
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,6 +27,11 @@ class User(UserMixin, db.Model):
             print("User {} doesn't have password_hash".format(self.username))
             return False
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 @login.user_loader
