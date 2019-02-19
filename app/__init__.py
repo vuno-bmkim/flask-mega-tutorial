ec085c711c77c5f146294d5ba6d4e49c3b7e3983
vuno-bmkim <bmkim@vuno.co>
2019-02-19 17:24:54 +0900

    Chapter 11: Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -10,6 +11,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
