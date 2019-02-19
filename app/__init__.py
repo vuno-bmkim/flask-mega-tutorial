46e47c295b0bfcbb83cb6c5a686802a8b4c8696f
vuno-bmkim <bmkim@vuno.co>
2019-02-19 17:07:12 +0900

    Chapter 10: Email Support

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
