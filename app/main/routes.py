0a0ac51347b5dee8892aec8875ce35b5ba0f3d49
vuno-bmkim <bmkim@vuno.co>
2019-02-21 18:32:25 +0900

    Chapter 20: Some JavaScript Magic

--- app/main/routes.py
+++ app/main/routes.py
@@ -77,6 +77,13 @@ def user(username):
                            title=_('Profile'))
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
+
+    
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
